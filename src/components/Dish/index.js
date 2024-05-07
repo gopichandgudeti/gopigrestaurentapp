2aaafc929b2d1079cd909ca17f4d4a488a77c3aa
gopichandgudeti <chandgudeti@gmail.com>
2024-05-07 07:16:50 +0000

    solution

---
+++ src/components/Dish/index.js
@@ -0,0 +1,39 @@
+import './index.css'
+
+const Dish = props => {
+  /*const {dishData} = props/*
+
+  /*const {
+    dishId,
+    dishName,
+    dishPrice,
+    dishImage,
+    dishCurrency,
+    dishCalories,
+    dishDescription,
+    dishAvailability,
+    dishType,
+    nexturl,
+    addonCat,
+  } = props */
+
+  /*const {addonCategory, addonCategoryId, addonSelection, nexturl, addons} =
+    addonCat
+  const {
+    dishId,
+    dishName,
+    dishPrice,
+    dishImage,
+    dishCurrency,
+    dishCalories,
+    dishDescription,
+    dishAvailability,
+    dishType,
+  } = addons */
+
+  return (
+          <p>soups</p>
+  )
+}
+
+export default Dish
